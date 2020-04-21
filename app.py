9684c5e31daeed4391832d32af8bd776bf1c47d2
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-04-21 21:50:06 +0530

    test api

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def dice_page():
 	return render_template('dice_page.html')
 
-#Testing of it works
+#Testing to check if it works
 @app.route('/test')
 def test():
     return "Works!"
