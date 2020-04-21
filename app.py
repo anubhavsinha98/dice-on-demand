d4cec919882b81273c494523ccc7fdc3d727a42e
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-04-21 20:46:05 +0530

    basic app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+#Dice Main page
+@app.route('/')
+def dice_page():
+	return render_template('dice_page.html')
+
+#Testing of it works
+@app.route('/test')
+def test():
+    return "Works!"
